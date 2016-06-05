78e3d450f00b9ee3081ee6f6a43b28a0d73c8d95
Jungwon.Shin <akracis@gmail.com>
2016-06-05 13:07:30 +0900

    email,email_UI 구현 완료 (xml 파일 전송) ※비밀번호 보안문제 이슈

---
+++ ANIMAL HUSBANDRY MANAGEMENT/excute4.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4.QtGui import QApplication, QMainWindow, QDialog
+from main4 import Ui_MainWindow  # here you need to correct the names
+from email4 import Ui_Dialog  # here you need to correct the names
+
+#excute mail window
+app = QApplication(sys.argv)
+window = QMainWindow()
+ui_main = Ui_MainWindow()
+ui_main.setupUi(window)
+
+#excute e-mail dialog
+dialog = QDialog();
+ui_email = Ui_Dialog()
+ui_email.setupUi(dialog)
+
+#set connection from button
+ui_main.btn_search.clicked.connect(ui_main.search)
+ui_main.btn_mail.clicked.connect(dialog.show)
+
+ui_email.btn_email_send.clicked.connect(ui_email.send)
+
+#show main window
+window.show()
+sys.exit(app.exec_())
